0aede353c4c3d609059cb8e506f66acd4579979e
Kim Jong Feel <DeveloperFEEL@gmail.com>
2024-08-04 23:48:17 +0900

    Check remain coins and restart next level

--- Coin_Dash/main.gd
+++ Coin_Dash/main.gd
@@ -35,3 +35,9 @@ func spawn_coins():
 			randi_range(0, screensize.x),
 			randi_range(0, screensize.y)
 			)
+
+func _process(delta):
+	if playing and get_tree().get_nodes_in_group("coins").size() == 0:
+		level += 1
+		time_left += 5
+		spawn_coins()

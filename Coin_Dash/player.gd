8d613a2724b70ee3377121d846099205da474243
Kim Jong Feel <DeveloperFEEL@gmail.com>
2024-08-04 13:15:58 +0900

    Set player within screen size

--- Coin_Dash/player.gd
+++ Coin_Dash/player.gd
@@ -11,4 +11,8 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	velocity = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	position += velocity * speed * delta
+	position.x = clamp(position.x, 0, screensize.x)
+	position.y = clamp(position.y, 0, screensize.y)
 	pass

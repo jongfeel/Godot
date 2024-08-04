3d934006391eec07c20fd2782db59d735c0d5bc2
Kim Jong Feel <DeveloperFEEL@gmail.com>
2024-08-04 13:16:45 +0900

    Set player within screen size with adjust sprite width and height

--- Coin_Dash/player.gd
+++ Coin_Dash/player.gd
@@ -3,6 +3,8 @@ extends Area2D
 @export var speed = 350
 var velocity = Vector2.ZERO
 var screensize = Vector2(480, 720)
+var spriteHalfWidth = 33 / 2
+var spriteHalfHeight = 32 / 2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,6 +15,6 @@ func _ready():
 func _process(delta):
 	velocity = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	position += velocity * speed * delta
-	position.x = clamp(position.x, 0, screensize.x)
-	position.y = clamp(position.y, 0, screensize.y)
+	position.x = clamp(position.x, spriteHalfWidth, screensize.x - spriteHalfWidth)
+	position.y = clamp(position.y, spriteHalfHeight, screensize.y - spriteHalfHeight)
 	pass

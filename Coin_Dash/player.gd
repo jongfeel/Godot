5f0ed8afe637b72eed835b33b7b4d3814097e0e8
Kim Jong Feel <DeveloperFEEL@gmail.com>
2024-08-04 13:41:17 +0900

    Add function: start(), die()

--- Coin_Dash/player.gd
+++ Coin_Dash/player.gd
@@ -27,3 +27,12 @@ func _process(delta):
 		$AnimatedSprite2D.flip_h = velocity.x < 0
 	
 	pass
+
+func start():
+	set_process(true)
+	position = screensize / 2
+	$AnimatedSprite2D.animation = "idle"
+	
+func die():
+	$AnimatedSprite2D.animation = "hurt"
+	set_process(false)

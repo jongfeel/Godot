6a794aa8ba5ac066e67930a0bd69f0f2e7c41ebc
Kim Jong Feel <DeveloperFEEL@gmail.com>
2024-08-05 19:22:17 +0900

    Add obstacle to game play

--- Coin_Dash/coin.gd
+++ Coin_Dash/coin.gd
@@ -16,3 +16,11 @@ func _ready():
 func _on_timer_timeout():
 	$AnimatedSprite2D.frame = 0
 	$AnimatedSprite2D.play()
+
+
+func _on_area_entered(area):
+	if area.is_in_group("obstacles"):
+		position = Vector2(
+			randi_range(0, screensize.x),
+			randi_range(0, screensize.y)
+			)

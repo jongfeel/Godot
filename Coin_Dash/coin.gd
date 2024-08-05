561037f27e6a4098bbd7802536f1b1d85fd5a08a
Kim Jong Feel <DeveloperFEEL@gmail.com>
2024-08-05 19:07:19 +0900

    Add coin animation different start frame each other

--- Coin_Dash/coin.gd
+++ Coin_Dash/coin.gd
@@ -9,3 +9,10 @@ func pickup():
 	tw.tween_property(self, "modulate:a", 0.0, 0.3)
 	await tw.finished
 	queue_free()
+	
+func _ready():
+	$Timer.start(randf_range(3, 8))
+
+func _on_timer_timeout():
+	$AnimatedSprite2D.frame = 0
+	$AnimatedSprite2D.play()

4dc1e33b696b8522ccac87d39136d81edafd6d27
Kim Jong Feel <DeveloperFEEL@gmail.com>
2024-08-09 10:43:27 +0900

    Implemntation ship shooting

--- SpaceRocks/Player/player.gd
+++ SpaceRocks/Player/player.gd
@@ -3,6 +3,11 @@ extends RigidBody2D
 @export var engine_power = 500
 @export var spin_power = 8000
 
+@export var bullet_scene : PackedScene
+@export var fire_rate = 0.25
+
+var can_shoot = true
+
 var thrust = Vector2.ZERO
 var rotation_dir = 0
 
@@ -14,8 +19,9 @@ var screensize = Vector2.ZERO
 func _ready():
 	change_state(ALIVE)
 	screensize = get_viewport_rect().size
+	$GunCooldown.wait_time = fire_rate
 
-func _process(delta):
+func _process(_delta):
 	get_input()
 	
 func change_state(new_state):
@@ -29,8 +35,10 @@ func get_input():
 	if Input.is_action_pressed("thrust"):
 		thrust = transform.x * engine_power
 	rotation_dir = Input.get_axis("rotate_left", "rotate_right")
+	if Input.is_action_pressed("shoot") and can_shoot:
+		shoot()
 	
-func _physics_process(delta):
+func _physics_process(_delta):
 	constant_force = thrust
 	constant_torque = rotation_dir * spin_power
 
@@ -39,3 +47,15 @@ func _integrate_forces(physics_state):
 	xform.origin.x = wrapf(xform.origin.x, 0, screensize.x)
 	xform.origin.y = wrapf(xform.origin.y, 0, screensize.y)
 	physics_state.transform = xform
+
+func shoot():
+	if state == INVULNERABLE:
+		return
+	can_shoot = false
+	$GunCooldown.start()
+	var b = bullet_scene.instantiate()
+	get_tree().root.add_child(b)
+	b.start($Muzzle.global_transform)
+
+func _on_gun_cooldown_timeout():
+	can_shoot = true

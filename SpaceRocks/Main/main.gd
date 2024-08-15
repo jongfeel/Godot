acfa916191c2957b7f2cb6ff44e616ff44b3125c
Kim Jong Feel <DeveloperFEEL@gmail.com>
2024-08-15 22:53:14 +0900

    Add UI script code in main scene

--- SpaceRocks/Main/main.gd
+++ SpaceRocks/Main/main.gd
@@ -4,10 +4,12 @@ extends Node
 
 var screensize = Vector2.ZERO
 
+var level = 0
+var score = 0
+var playing = false
+
 func _ready():
 	screensize = get_viewport().get_visible_rect().size
-	for i in 3:
-		spawn_rock(3)
 		
 func spawn_rock(size, pos=null, vel=null):
 	if pos == null:
@@ -22,6 +24,7 @@ func spawn_rock(size, pos=null, vel=null):
 	r.exploded.connect(self._on_rock_exploded)
 
 func _on_rock_exploded(size, radius, pos, vel):
+	score += 10 * size
 	if size <= 1:
 		return
 	for offset in [-1, 1]:
@@ -30,4 +33,29 @@ func _on_rock_exploded(size, radius, pos, vel):
 		var newvel = dir * vel.length() * 1.1
 		spawn_rock(size - 1, newpos, newvel)
 		
+func new_game():
+	get_tree().call_group("rocks", "queue_free()")
+	level = 0
+	score = 0
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready!")
+	$Player.reset()
+	await $HUD/Timer.timeout
+	playing = true
+	
+func new_level():
+	level += 1
+	$HUD.show_message("Wave %s" % level)
+	for i in level:
+		spawn_rock(3)
+
+func _process(delta):
+	if not playing:
+		return
+	if get_tree().get_nodes_in_group("rocks").size() == 0:
+		new_level()
+		
+func game_over():
+	playing = false
+	$HUD.game_over()
 	

6e7b73ccaf88c4d037d9b8c5f7a6890e5734f77c
Kim Jong Feel <DeveloperFEEL@gmail.com>
2024-08-18 18:13:59 +0900

    Create PathFollow2D and set to random EnemyPath

--- SpaceRocks/Enemy.gd
+++ SpaceRocks/Enemy.gd
@@ -1,5 +1,25 @@
 extends Area2D
 
+@export var bullet_scene : PackedScene
+@export var speed = 150
+@export var rotation_speed = 120
+@export var health = 3
+
+var follow = PathFollow2D.new()
+var target = null
+
+func _ready():
+	$Sprite2D.frame = randi() % 3
+	var path = $EnemyPaths.get_children()[randi() % $EnemyPaths.get_child_count()]
+	path.add_child(follow)
+	follow.loop = false
+	
+func _physics_process(delta):
+	rotation += deg_to_rad(rotation_speed) * delta
+	follow.progress += speed * delta
+	position = follow.global_position
+	if follow.progress_ratio >= 1:
+		queue_free()
 
 func _on_gun_cool_down_timeout():
 	pass # Replace with function body.

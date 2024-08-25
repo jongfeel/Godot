df01dc39b4146be8f5a8f6ce806314a9f27dc4c8
Kim Jong Feel <DeveloperFEEL@gmail.com>
2024-08-26 00:15:25 +0900

    Add player movement

--- junglejump/Player/player.gd
+++ junglejump/Player/player.gd
@@ -23,3 +23,42 @@ func change_state(new_state):
 			$AnimationPlayer.play("jump_up")
 		DEAD:
 			hide()
+
+func get_input():
+	var right = Input.is_action_pressed("right")
+	var left = Input.is_action_pressed("left")
+	var jump = Input.is_action_pressed("jump")
+	
+	velocity.x = 0
+	if right:
+		velocity.x += run_speed
+		$Sprite2D.flip_h = false
+	if left:
+		velocity.x -= run_speed
+		$Sprite2D.flip_h = true
+	if jump and is_on_floor():
+		change_state(JUMP)
+		velocity.y = jump_speed
+	if state == IDLE and velocity.x != 0:
+		change_state(RUN)
+	if state == RUN and velocity.x == 0:
+		change_state(IDLE)
+	if state in [IDLE, RUN] and !is_on_floor():
+		change_state(JUMP)
+		
+func _physics_process(delta):
+	velocity.y += gravity * delta
+	get_input()
+	
+	move_and_slide()
+	
+	if state == JUMP and is_on_floor():
+		change_state(IDLE)
+		
+	if state == JUMP and velocity.y	> 0:
+		$AnimationPlayer.play("jump_down")
+		
+func reset(_position):
+	position = _position
+	show()
+	change_state(IDLE)

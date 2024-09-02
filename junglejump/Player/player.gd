0edab6c3195ebbd5ba644bca998f6c60ed81f8f1
Kim Jong Feel <DeveloperFEEL@gmail.com>
2024-09-02 17:36:06 +0900

    Change logic flow in player.gd

--- junglejump/Player/player.gd
+++ junglejump/Player/player.gd
@@ -36,6 +36,9 @@ func change_state(new_state):
 			hide()
 
 func get_input():
+	if state == HURT:
+		return
+
 	var right = Input.is_action_pressed("right")
 	var left = Input.is_action_pressed("left")
 	var jump = Input.is_action_pressed("jump")
@@ -50,8 +53,6 @@ func get_input():
 	if jump and is_on_floor():
 		change_state(JUMP)
 		velocity.y = jump_speed
-	if state == HURT:
-		return
 	if state == IDLE and velocity.x != 0:
 		change_state(RUN)
 	if state == RUN and velocity.x == 0:
@@ -65,12 +66,6 @@ func _physics_process(delta):
 	
 	move_and_slide()
 	
-	if state == JUMP and is_on_floor():
-		change_state(IDLE)
-		
-	if state == JUMP and velocity.y	> 0:
-		$AnimationPlayer.play("jump_down")
-		
 	if state == HURT:
 		return
 		
@@ -84,6 +79,12 @@ func _physics_process(delta):
 				velocity.y = -200
 			else:
 				hurt()
+	
+	if state == JUMP and is_on_floor():
+		change_state(IDLE)
+		
+	if state == JUMP and velocity.y	> 0:
+		$AnimationPlayer.play("jump_down")
 		
 func reset(_position):
 	position = _position
